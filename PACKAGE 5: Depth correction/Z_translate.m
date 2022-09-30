333e8801f1bd6e4300f49f40829cec6a9a0eb7f4
marylkraft <114761992+marylkraft@users.noreply.github.com>
2022-09-30 18:47:18 -0500

    Update Z_translate.m
    Addition of copyright notice and license information.

--- PACKAGE 5: Depth correction/Z_translate.m
+++ PACKAGE 5: Depth correction/Z_translate.m
@@ -1,3 +1,8 @@
+% Copyright 2022 University of Illinois Board of Trustees. All Rights Reserved.
+% These codes were authored by Melanie A. Brunet and Brittney L. Gorman under the supervision of Mary L. Kraft. 
+% This file is part of KraftLab.github.io repository, which is released under specific terms.  
+% See file License.txt file or go to https://creativecommons.org/licenses/by/4.0/ for full license details.
+
 function ZTrans = Z_translate(M,depth,Topo,Multi)
 %% Depth correction of ion images   
 % written by Brittney Gorman
